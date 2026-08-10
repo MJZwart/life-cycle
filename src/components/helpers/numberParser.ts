a048618379727e230cec3ed5ff4a76a3fa8f81a5
Mitchell Zwart <mitchellzwart91@gmail.com>
2026-08-10 14:04:43 +0200

    Added decimal points to number parser

--- src/components/helpers/numberParser.ts
+++ src/components/helpers/numberParser.ts
@@ -5,8 +5,9 @@ export const parseAge = (days: number) => {
     return `${years} years, ${Math.floor(days - (years * 365))} days`;
 }
 
-const NUMBER_SHORTHAND_CUTOFF = 10000;
+const NUMBER_SHORTHAND_CUTOFF = 10_000;
 
+const THOUSAND = 1_000;
 const MILLION = 1_000_000;
 const BILLION = 1_000_000_000;
 const TRILLION = 1_000_000_000_000;
@@ -16,10 +17,10 @@ const QUINTILLION = 1_000_000_000_000_000_000;
 export const parseThousands = (num: number): string => {
     if (num < NUMBER_SHORTHAND_CUTOFF) return num.toString();
 
-    if (num < MILLION) return Math.round(num / 1000) + 'k';
-    else if (num < BILLION) return Math.round(num / MILLION) + 'm';
-    else if (num < TRILLION) return Math.round(num / BILLION) + 'b';
-    else if (num < QUADRILLION) return Math.round(num / TRILLION) + 't';
-    else if (num < QUINTILLION) return Math.round(num / QUADRILLION) + 'q';
-    else return Math.round(num / QUINTILLION) + 'Q';
+    if (num < MILLION) return Math.round((num / THOUSAND) * 100) / 100 + 'k';
+    else if (num < BILLION) return Math.round((num / MILLION) * 100) / 100 + 'm';
+    else if (num < TRILLION) return Math.round((num / BILLION) * 100) / 100 + 'b';
+    else if (num < QUADRILLION) return Math.round((num / TRILLION) * 100) / 100 + 't';
+    else if (num < QUINTILLION) return Math.round((num / QUADRILLION) * 100) / 100 + 'q';
+    else return Math.round((num / QUINTILLION) * 100) / 100 + 'Q';
 }
